fc747be557f39d3c5052728836c737d684680d2d
B1QUAD <accntmail.745@gmail.com>
2021-01-23 02:48:48 -0600

    First prototype is functional

--- simpleServer.js
+++ simpleServer.js
@@ -1,21 +1,54 @@
 var http = require('http'); // Import Node.js core module
 var fs = require('fs');
 
+const hostDir = "hostDir";
 var server = http.createServer(function (req, res) {   //create web server
 
-        console.log("Incoming Requst URL: " + req.url);
-        if(req.url.includes("..")) {
-                res.write("Invalid Request");
+
+        console.log("Incoming Requst URL: " + req.url + "\nIncoming Request Path: " + hostDir + req.url);
+        if (req.url.includes("..")) {
+                res.write("Invalid Request, malicious");
         } else {
+        }
+
+        if (true) {
+
                 try {
-                        res.write(fs.readFile(req.url));
+                        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+                        res.write(parseURL(req.url));
+                        res.end();
                 } catch (e) {
-                        res.write("Invalid Request: " + req.url);
+                        res.writeHead(200, { 'Content-Type': 'text/html' });
+                        console.log(e)
+                        //res.write("Invalid Request - exception: " + req.url);
+                        res.end();
                 }
         }
-        res.write("oof?");
-        res.end();
 });
+
+function parseURL(url) {
+
+        // Malformed URL checks
+        if (url.includes("..")) {
+                callback(new Error(ex.toString()));
+        }
+
+        if (url === "/") {
+                // console.log("testing " + fs.readFile(hostDir + "index.html"));
+                return readF(hostDir + url + "index.html");
+        } else {
+                return readF(hostDir + url + ".html");
+        }
+}
+
+function readF(input) {
+        console.log("# Reading file: " + input);
+        var content;
+
+        content = fs.readFileSync(input, "utf8").toString();
+        return content;
+}
+
 server.listen(5000); //6 - listen for any incoming requests
 
 console.log('Node.js web server at port 5000 is running..');
